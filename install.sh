b51e3ac9bbde0efdcc41f716305657fc5d1a2115
Ibrahim Johar <ibbe.johar@gmail.com>
2022-06-02 16:02:41 +0200

    added void linux in the script

--- install.sh
+++ install.sh
@@ -7,189 +7,188 @@ PURPLE='\033[0;35m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-function main () {
 
-function privelige(){
-	if [ "$prev_elavator" = "1" ]
-	then
-		echo "Installing doas"
-		sudo pacman -S opendoas --noconfirm
-		sudo touch /etc/doas.conf
-		echo "permit nopass :wheel" | sudo tee /etc/doas.conf
-		doas pacman -Rns sudo --noconfirm
-		doas ln -s /usr/bin/doas /usr/bin/sudo
-    elif [ "$prev_elavator" = "2" ]
-    then
-		# Add pound symbol at the start of line 82
-		# and remove pound symbol at start of line 85
-		sudo bash -c "sed -i '82 s/^/#/' /etc/sudoers && sed -i '85 s/^#//' /etc/sudoers"
-    else 
-		echo -e "${RED}This postinstall script will not work if the privelige elavator is not choosen"
-		sleep 3
-		exit
-	fi
+function main(){
+
+    function privelige(){
+	    if [ "$prev_elavator" = "1" ]
+	    then
+			echo "Installing doas"
+			sudo pacman -S opendoas --noconfirm
+			sudo touch /etc/doas.conf
+			echo "permit nopass :wheel" | sudo tee /etc/doas.conf
+			doas pacman -Rns sudo --noconfirm
+			doas ln -s /usr/bin/doas /usr/bin/sudo
+    	elif [ "$prev_elavator" = "2" ]
+    		then
+			# Add pound symbol at the start of line 82
+			# and remove pound symbol at start of line 85
+			sudo bash -c "sed -i '82 s/^/#/' /etc/sudoers && sed -i '85 s/^#//' /etc/sudoers"
+    	else 
+			echo -e "${RED}This postinstall script will not work if the privelige elavator is not choosen"
+			sleep 3
+			exit
+		fi
 }
 
-function xdg(){
-	sudo pacman -S xdg-user-dirs --noconfirm
-	xdg-user-dirs-update
-}
 
-function Dotfiles(){
-	cd $HOME
-	rm .bashrc
-	rm .bash_profile
-	git clone https://github.com/ibbejohar/.dotfile.git
-	cd .dotfile
-	./config.sh
-}
+	function xdg(){
+		sudo pacman -S xdg-user-dirs --noconfirm
+		xdg-user-dirs-update
+	}
 
-function packages(){
-
-	program="
-	alacritty
-	aria2
-	bitwarden
-	curl
-	exa
-	feh
-	firefox
-	fuse
-	git
-	mpv
-	nemo
-	neofetch
-	neovim
-	nodejs
-	ntfs-3g
-	pulseaudio
-	pulsemixer
-	rofi
-	sxiv
-	unclutter
-	unrar
-	unzip
-	zathura
-	zathura-pdf-mupdf
-	zip
-	"
-	sudo pacman -S $program --noconfirm
-}
-
-function aur_helper(){
-
-	aur_packages="
-	ani-cli
-	devour
-	ly
-	mangodl
-	nerd-fonts-complete
-	"
-	if [ "$aur" = "1" ]
-	then
-		git clone https://aur.archlinux.org/yay.git
-		cd yay
-		makepkg -si --noconfirm
-		cd ..
-		rm -rf yay
-	elif [ "$aur" = "2" ]
-	then
-		git clone https://aur.archlinux.org/paru.git
-		cd paru
-		makepkg -si --noconfirm
-		cd ..
-		rm -rf paru
-	else
-		echo "No AUR helper was selected"
-	fi
-
-	$aur -S $aur_packages --noconfirm
-
-}
-
-function add_packages(){
-	git clone https://github.com/CoolnsX/dra-cla
-	cd dra-cla
-	sudo cp dra-cla /usr/local/bin/dra-cla
-	cd ..
-	rm -rf dra-cla
-}
-
-function install_wm(){
-
-		# dwm
-	if [ "$wm" = "1" ]
-	then
-		mkdir -p $HOME/.config/suckless
-		cd $HOME/.config/suckless
-		git clone https://github.com/ibbejohar/dwm.git
-		git clone https://github.com/ibbejohar/blocks.git
-
-		cd dwm
-		sudo make clean install
-		cd ..
-		cd blocks
-		sudo make clean install
-		./install.sh
-		cd $HOME	
-
-		# spectrwm
-	elif [ "$wm" = "2" ]
-	then
-		sudo pacman -S spectrwm
-		cd $HOME/.config
-		git clone https://github.com/ibbejohar/spectrwm.git
+	function Dotfiles(){
 		cd $HOME
-	else
-		echo "No window manager was selected"
-	fi
+		rm .bashrc
+		rm .bash_profile
+		git clone https://github.com/ibbejohar/.dotfile.git
+		cd .dotfile
+		./config.sh
+	}
+
+	function add_packages(){
+		git clone https://github.com/CoolnsX/dra-cla
+		cd dra-cla
+		sudo cp dra-cla /usr/local/bin/dra-cla
+		cd ..
+		rm -rf dra-cla
+	}
+
+	function install_wm(){
+
+			# dwm
+		if [ "$wm" = "1" ]
+		then
+			mkdir -p $HOME/.config/suckless
+			cd $HOME/.config/suckless
+			git clone https://github.com/ibbejohar/dwm.git
+			git clone https://github.com/ibbejohar/blocks.git
+
+			cd dwm
+			sudo make clean install
+			cd ..
+			cd blocks
+			sudo make clean install
+			./install.sh
+			cd $HOME	
+
+			# spectrwm
+		elif [ "$wm" = "2" ]
+		then
+			sudo pacman -S spectrwm
+			cd $HOME/.config
+			git clone https://github.com/ibbejohar/spectrwm.git
+			cd $HOME
+		else
+			echo "No window manager was selected"
+		fi
+
+	}
+
+	function hardware_disable(){
+		sudo touch /etc/modprobe.d/nobeep.conf
+		echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
+	}
+
+	function mounting_point(){
+		sudo mkdir -p /media/"$drive"
+
+	}
+
+	function clean_up(){
+		if [ "$prev_elavator" = "1" ]
+		then
+			echo "permit persist :wheel" | sudo tee /etc/doas.conf
+		elif [ "$prev_elavator" = "2" ]
+		then
+			# Add pound symbol at the start of line 85
+			# and remove pound symbol at the start of line 82
+			sudo bash -c "sed -i '85 s/^/#/' /etc/sudoers && sed -i '82 s/^#//' /etc/sudoers"
+		else
+			echo "NULL"
+		fi
+
+	}
 
 }
 
 
-function services(){
-	sudo systemctl enable ly.service
+function arch () {
 
-}
+	function arch_pac(){
 
-function hardware_disable(){
-	sudo touch /etc/modprobe.d/nobeep.conf
-	echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
-}
+		program="
+		alacritty
+		aria2
+		bitwarden
+		curl
+		exa
+		feh
+		firefox
+		fuse
+		git
+		mpv
+		nemo
+		neofetch
+		neovim
+		nodejs
+		ntfs-3g
+		pulseaudio
+		pulsemixer
+		rofi
+		sxiv
+		unclutter
+		unrar
+		unzip
+		zathura
+		zathura-pdf-mupdf
+		zip
+		"
+		sudo pacman -S $program --noconfirm
+	}
 
-function mounting_point(){
-	sudo mkdir -p /media/"$drive"
+	function aur_helper(){
 
-}
+		aur_packages="
+		ani-cli
+		devour
+		ly
+		mangodl
+		nerd-fonts-complete
+		"
+		if [ "$aur" = "1" ]
+		then
+			git clone https://aur.archlinux.org/yay.git
+			cd yay
+			makepkg -si --noconfirm
+			cd ..
+			rm -rf yay
+		elif [ "$aur" = "2" ]
+		then
+			git clone https://aur.archlinux.org/paru.git
+			cd paru
+			makepkg -si --noconfirm
+			cd ..
+			rm -rf paru
+		else
+			echo "No AUR helper was selected"
+		fi
 
-function clean_up(){
-	if [ "$prev_elavator" = "1" ]
-    then
-		echo "permit persist :wheel" | sudo tee /etc/doas.conf
-    elif [ "$prev_elavator" = "2" ]
-    then
-		# Add pound symbol at the start of line 85
-		# and remove pound symbol at the start of line 82
-		sudo bash -c "sed -i '85 s/^/#/' /etc/sudoers && sed -i '82 s/^#//' /etc/sudoers"
-    else
-		echo "NULL"
-    fi
+		$aur -S $aur_packages --noconfirm
 
-}
+	}
 
-### Executing funtions ###
 
-privelige
-xdg
-Dotfiles
-packages
-aur_helper
-add_packages
-install_wm
-services
-hardware_disable
-mounting_point
-clean_up
+	function systemd(){
+		sudo systemctl enable ly.service
+
+	}
+
+	### Executing Functions ###
+	arch_pac
+	aur_helper
+	systemd
+
 }
 
 function automation(){
@@ -224,15 +223,32 @@ function not_root(){
 }
 
 
+function linux_distro(){
+	if [ "$distro" = "1" ]
+	then
+		pkg="pacman -S --noconfirm"
+	elif [ "$distro" = "2" ]
+	then
+		pkg="xbps-install -Sy"
+	fi
+}
+
+
+
+
 not_root
 echo -e "${PURPLE}Welcome To Arch Post Installation"
 echo " "
-echo "Choosing the Automatic option, the script will choose for you."
+echo -e "${RED}TYPE ONLY THE NUMBER"
+echo -e "${PURPLE}Choose the distro"
+echo -e "${NC}1. Arch   2. Void"
+read distro
+echo " "
+echo -e "${PURPLE}Choosing the Automatic option, the script will choose for you."
 echo "Choosing the Manual way, you will have two options in every questons that follow."
 echo " "
 sleep 3
-echo -e "${RED}TYPE ONLY THE NUMBER"
-echo -e "${PURPLE}Choose one of the following"
+echo  "Choose one of the following"
 echo -e "${NC}1. Automatic    2. Manual"
 read action
 automation
